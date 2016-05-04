39cd5d5b874f06247787deb4a00cff3d18998bf5
Michael O. Church <michael.o.church@gmail.com>
2016-05-04 07:23:53 -0500

    first working draft of language (w/out macros)

--- src/setbang/core.clj
+++ src/setbang/core.clj
@@ -1,11 +1,8 @@
 (ns setbang.core
-  (:require [clojure.set :as clj-set]))
+  (:require [clojure.set :as clj-set]
+            [clojure.string :as clj-string]))
 
-;; TODO: FIX THIS
-;; Set = sequence of includes and excludes...
-;; ... possibly finite.
-;; ... treat integers as ur-elements?
-
+;; Only exists for testing. Generates a non-normalized integer-equivalent set.
 (defn int-to-set [n]
   (loop [i 0 acc #{}]
     (if (>= i n) acc (recur (inc i) (conj acc acc)))))
@@ -17,206 +14,165 @@
             (if (= s (into #{} (range (count x))))
               (count x)
               s))
-        :else (map (fn [[_ e]] (normalize e)) x)))
+        :else (map normalize x)))
 
-(def omega (map #(vector :include %) (range)))
+(defn is-mumber? [x]
+  (number? (normalize x)))
 
-(def loud-empty (map #(vector :exclude %) (range)))
+(defn as-set [x]
+  (cond
+   (number? x) (into #{} (range x))
+   (set?    x) x
+   :else       (into #{} x)))
 
+(def omega (range))
+
+;; bottom : extreme pathological set. Empty but yields no information. Only used
+;; for testing.
 (def bottom (lazy-seq (loop [] (recur))))
 
 ;; we should be able to show "there are at least 20 primes"
 ;; e.g. (primes)#(20)?
 
-(defn force-set [x]
-  (cond (number? x) x
-        (set?    x) x
-        :else (into #{}
-                    (mapcat (fn [[inc-or-exc v]]
-                              (when (= inc-or-exc :include) [v])) x))))
 
 (declare member-check)
 
 (defn equality-check [x1 x2]
-  (let [check-diff (fn [[inc-or-exc elt2] elt1]
-                     (if (= inc-or-exc :exclude)
-                       (member-check elt1 elt2)
-                       (not (member-check elt1 elt2))))]
-    (cond
-     (identical? x1 x2) true
-     (and (or (set? x1) (number? x1))
-          (or (set? x2) (number? x2))) (= x1 x2)
-     (or (set? x1) (number? x1))
-       (and (not (some #(check-diff % x1) x2))
-            (= x1 (normalize (force-set x2))))
-     (or (set? x2) (number? x2))
-       (and (not (some #(check-diff % x2) x1))
-            (= x2 (normalize (force-set x1))))
-
-     :else
-       (loop [i1 #{} e1 #{} i2 #{} e2 #{} ctr 0 remt1 x1 remt2 x2]
-         (if (and (empty? remt1) (empty? remt2))
-           (equality-check (normalize i1) (normalize i2))
-           (if (even? ctr)
-             (let [[c v] (first remt1)]
-               (cond (and (= c :include) (some #(equality-check % v) e2))
-                       false
-                     (and (= c :exclude) (some #(equality-check % v) i2))
-                       false 
-                     (= c :include) (recur (conj i1 v) e1 i2 e2 (inc ctr) (next remt1) remt2)
-                     (= c :exclude) (recur i1 (conj e1 v) i2 e2 (inc ctr) (next remt1) remt2)
-                     (= c nil)      (recur i1 e1 i2 e2 (inc ctr) (next remt1) remt2)))
-             (let [[c v] (first remt2)]
-               (cond (and (= c :include) (some #(equality-check % v) e1))
-                       false
-                     (and (= c :exclude) (some #(equality-check % v) i1))
-                       false 
-                     (= c :include) (recur i1 e1 (conj i1 v) e2 (inc ctr) remt1 (next remt2))
-                     (= c :exclude) (recur i1 e1 i2 (conj e2 v) (inc ctr) remt1 (next remt2))
-                     (= c nil)      (recur i1 e1 i2 e2 (inc ctr) remt1 (next remt2))))))))))
+  (if (and (number? x1) (number? x2))
+    (= x1 x2)
+    (let [s1 (if (number? x1) (into #{} (range x1)) x1)
+          s2 (if (number? x2) (into #{} (range x2)) x2)]
+      (if (set? s1)
+        (and (every? #(member-check s1 %) s2)
+             (every? #(member-check s2 %) s1))
+        (and (every? #(member-check s2 %) s1)
+             (every? #(member-check s1 %) s2))))))
 
 (defn member-check [x e]
+  (cond (number? x) (and (number? e) (< e x))
+        (set? x)    (if (or (number? e) (set? e)) (x e)
+                        (some #(equality-check % e) x))
+        :else       (some #(equality-check % e) x)))
+
+(defn pair [x1 x2]
+  (normalize (hash-set x1 x2)))
+
+(defn choice [x]
   (cond
-   (number? x) (and (number? e) (< e x))
-   (set?    x)
-     (some #(equality-check % e) x)
-   :else
-     (->> x
-          (filter #(equality-check (second %) e))
-          ffirst
-          (= :include))))
+   (= x 0) [0 0]
+   (number? x) (choice (into #{} (range x)))
+   (set? x) (let [elt (first x)
+                  remt (if (empty? (rest x)) 0 (into #{} (rest x)))]
+              [(normalize remt) elt])
+   :else (if (empty? x)
+           [0 0]
+           (let [elt (first x)]
+             [(rest x) elt]))))
 
-;; (defn int-to-set [n]
-;;   (loop [acc #{}, i 0]
-;;     (if (>= i n) acc (recur (conj acc acc) (inc i)))))
+(defn is-empty-set [x]
+  (cond
+   (number? x) (= x 0)
+   (set?    x) (empty? x)
+   :else       (empty? x)))
 
-;; (defn equality-1 [n s]
-;;   (cond
+(defn choose-many [x n]
+  (let [s (if (number? x) (into #{} (range x)) x)]
+    (loop [remt s out #{} i 0]
+      (if (or (>= i n) (is-empty-set remt))
+        [(normalize remt) (normalize out)]
+        (let [[remt-1 e] (choice remt)]
+          (recur remt-1 (conj out e) (inc i)))))))
 
-;; (defn gen-count [x]
-;;   (cond (number? x) x
-;;         (set?    x) (count x)
-;;         :else       Long/MAX_VALUE))
+;; FIXME: insert should reorder inf. sets to avoid long equality-checks.
+(defn insert [x e]
+  (cond
+   (and (number? x) (number? e) (< e x)) x
+   (and (number? x) (number? e) (= e x)) (+ x 1)
+   (or (number? x) (set? x))
+     (let [s (if (number? x) (into #{} (range x)) x)]
+       (normalize (conj s e)))
+   :else (cons e (remove #(equality-check e (second %)) x))))
 
-;; (defn normalize [x]
-;;   (if (number? x) x
-;;       (let [s (sort-by gen-count (map normalize x))]
-;;         (if (= s (range (count s)))
-;;           (count s)
-;;           (into #{} s)))))
+(defn has-definite-size? [x]
+  (or (number? x) (set? x)))
 
-;; (defn normalize [x]
-;;   (if (number? x) x
-;;       (let [s (into #{} (map normalize x))
-;;             n (count s)]
-;;         (if (= s (into #{} (range n))) n s))))
-;; ;;      (map normalize n
+;; Non-terminating on an infinite set. 
+(defn get-size-up-to [x n]
+  (cond (number? x) x
+        (set?    x) (count x)
+        :else       (count (take n x))))
 
-;; (defn mem-check [s e]
-;;   )
+(defn gen-union-2 [x1 x2]
+  (cond
+   (and (number? x1) (number? x2)) (max x1 x2)
+   (and (has-definite-size? x1) (has-definite-size? x2))
+     (normalize (clj-set/union (as-set x1) (as-set x2)))
+     :else
+       (if (is-empty-set x1)
+         x2
+         (lazy-seq
+          (let [[x' e] (choice x1)]
+            (cons e (remove #(equality-check % e) (gen-union-2 x2 x'))))))))
 
-;; (defn eq-check [s1 s2]
-;;   (cond
-;;    (number? s1) 
+(defn as-seq [x]
+  (cond
+   (number? x) (range x)
+   (set? x)    (seq x)
+   :else       x))
 
+;; [x1 x2 x3 ...] [y1 y2 y3 ... ] [z1 z2 z3 ...] [w1 ...] ->
+;; x1 x2 y1 x3 y2 z1 x4 y3 z2 w1 ...
+;; "diagonal" approach to make sure everything gets picked. 
+(defn stagger [xs & [n]]
+  (if (empty? xs) ()
+      (let [n (or n 1)
+            choices (map choice (take n xs))]
+        (concat (map second choices)
+                (stagger (concat (remove is-empty-set (map first choices))
+                                 (drop n xs))
+                         (inc n))))))
 
+(defn staggered-union [xs]
+  (lazy-seq
+   (let [ys (stagger xs)
+         y1 (first ys)]
+     (cons y1 (remove #(equality-check % y1) ys)))))
 
+(defn gen-union [x-set]
+  (cond
+   (number? x-set) x-set
+   (set?    x-set) (reduce gen-union-2 #{} x-set)
+   :else (staggered-union x-set)))
 
+(defn gen-filter [f x]
+  (cond
+   (number? x) (into #{} (filter f (range x)))
+   (set?    x) (into #{} (filter f x))
+   :else       (filter f x)))
 
+(defn gen-intersect-2 [x1 x2]
+  (cond
+   (and (number? x1) (number? x2)) (min x1 x2)
+   (has-definite-size? x1) (gen-filter #(member-check x2 %) x1)
+   (has-definite-size? x2) (gen-filter #(member-check x1 %) x2)
+   :else (gen-filter #(member-check x1 %) x2)))
 
-;; Sets can be represented in 3 ways.
-;; integer : N
-;; set     : #{...}
-;; lazy    : {:sequence (...) lazy
-;;            :includes #{...}
-;;            :excludes #{...}
-;;            :include? <>    fn -> bool}
+(defn difference-2 [x1 x2]
+  (gen-filter #(not (member-check x2 %)) x1))
 
-;; (defn non-termination []
-;;   (throw (Exception. "Non-termination detected.")))
+(defn exclusive-or-2 [x1 x2]
+  (gen-union-2 (difference-2 x1 x2) (difference-2 x2 x1)))
 
-(defn int-as-set [n]
-  (into #{} (range n)))
+(defn gen-union-map [f x]
+  (cond
+   (number? x) (gen-union (into #{} (map f (range x))))
+   (set?    x) (gen-union (into #{} (map f x)))
+   :else       (gen-union (map f x))))
 
-(defn vec-drop [k v]
-  (subvec v 0 (- (count v) k)))
-
-(defn to-int [x]
-  (cond (integer? x) x
-        (set?     x)
-          (let [n (count x)] (when (= x (set (range n))) n))
-        :else (non-termination)))
-
-(defn convert-if-int [x]
-  (or (to-int x) x))
-
-(defn left-pad [size stack]
-  (into [] (concat (repeat (- size (count stack)) 0) stack)))
-
-;; (defn lazy-set-choice [{:keys [sequence includes excludes include?] :as x}]
-;;   (if (empty? includes)
-;;     (let [reject-fn #(or (not (include? %)) (excludes %))
-;;           [rejects seq1] (split-with reject-fn sequence)
-;;           choice (first seq1)]
-;;       [{:sequence (rest seq1), :include? include?
-;;         :includes #{}
-;;         :excludes (clj-set/union excludes (set rejects) #{choice})}
-;;        choice])
-;;     (let [choice (first includes)]
-;;       [(-> x (update :includes disj choice)
-;;            (update :excludes conj choice))
-;;        choice])))
-
-;; (defn lazy-set-seq [lazy-set]
-;;   (lazy-seq
-;;    (let [[remt choice] (lazy-set-choice lazy-set)]
-;;      (cons choice (lazy-set-seq remt)))))
-
-;; (defn choice [x]
-;;   (cond (integer? x) (do-choice (int-as-set x))
-;;         (set?     x) (let [c (first x)] [(disj x c) c])
-;;         :else (lazy-set-choice x)))
-                
-;; (defn lazy-set-insert [x e]
-;;   (-> (update x :includes conj e)
-;;       (update :excludes disj e)))
-
-;; ;; insert has a bug. lazy-set-insert has something wrong with it.
-;; ;; Also, the :includes and :excludes have rely on set equality. 
-;; (defn insert [x e]
-;;   (cond (integer? x)
-;;           (when-let [n (to-int e)]
-;;             (cond (= n x) (inc x)
-;;                   (< n x) x
-;;                   :else (conj (int-as-set x) e)))
-;;         (set? x) (conj x e)
-;;         :else (lazy-set-insert x e)))
-
-
-;; (defn pair [x y]
-;;   (let [s (hash-set x y)]
-;;     (cond (= s #{0}) 1
-;;           (= s #{0 1}) 2
-;;           :else s)))
-
-;; (defn set-inc [x]
-;;   (cond (integer? x) (+ x 1)
-;;         (set? x)     (conj x x)
-;;         :else        (update x :strict conj x)))
-
-;; (def force-number 1024)
-
-;; (def omega {:excludes #{}, :includes #{}, :include? (constantly true), :sequence (range)})
-
-;; ;; Stack is irrelevant for nats (it's a constant).
-;; (def nats {:strict (set (range force-number)) :lazy (iterate inc force-number) :stack nil})
-
-(defn card [x & [stack]]
+(defn card [x]
   (cond (integer? x) x
         (set? x)     (count x)
-        :else        (let [{:keys [strict lazy stack]} x
-                           n (count strict)]
-                       {:strict (set (range n)) :lazy (iterate inc n) :stack stack})))
+        :else (normalize (map-indexed (fn [i e] i) x))))
 
 (defn select-by-bits [n coll]
   (into #{}
@@ -224,42 +180,184 @@
                 (map-indexed (fn [i x] (when (bit-test n i) x)) coll))))
 
 (defn power-set-seq [coll]
-  (let [limit 40 ;; We'll never generate more than 2^40 things.
+  (let [limit 60 ;; We'll never generate more than 2^60 things.
         coll' (take limit coll)]
-    (map #(select-by-bits % coll') (range 0 (bit-shift-left 1 (count coll'))))))
+    (map #(normalize (select-by-bits % coll')) (range 0 (bit-shift-left 1 (count coll'))))))
 
-;; (defn power-set [x]
-;;   (cond (integer? x) (
+(def large-set-cutoff 100000)
 
-;; (defn set-to-seq [x]
-;;   (cond (number? x) (range x)
-;;         (set? x)    (seq x)
-;;         :else (let [{:keys [strict lazy]} x]
-;;                 (concat (seq strict) lazy))))
+(defn shrink-if-possible [x]
+  (cond (number? x) x
+        (set?    x) x
+        :else (let [z (take (inc large-set-cutoff) x)]
+                (if (<= (count z) large-set-cutoff)
+                  (normalize (into #{} z))
+                  x))))
 
-;; (declare equality-check)
-;; (defn member-check [x e]
-;;   (cond (number? x) (> x (to-int e))
-;;         (set? x)    (some #(equality-check % e) x)
-;;         :else       (some #(equality-check % e) (set-to-seq x))))
+(defn power-set [x]
+  (let [n (get-size-up-to x 17)
+        s (if (number? x) (range x) x)]
+    (if (> n 16)
+      (power-set-seq s)
+      (into #{} (power-set-seq s)))))
 
-;; (defn equality-check [x1 x2]
-;;   (if (and (number? x1) (number? x2)) (= x1 x2)
-;;       (let [y1 (if (number? x1) (int-as-set x1) x1)
-;;             y2 (if (number? x2) (int-as-set x2) x2)]
-;;         (cond (and (set? y1) (set? y2)) (= y1 y2)
-;;               (set? y1) (not (some #(not (member-check y1 %)) (set-to-seq y2)))
-;;               (set? y2) (not (some #(not (member-check y2 %)) (set-to-seq y1)))
-;;               :else (if (identical? y1 y2) true (non-termination))))))
+(defn ordered-pair [x y]
+  (normalize (hash-set (hash-set x) (hash-set x y))))
 
-;; (defn set-intersect [x1 x2]
-;;   (cond (and (number? x1) (number? x2)) (min x1 x2)
-;;         (let [y1 (if (number? x1) (int-as-set x1) x1)
-;;               y2 (if (number? x2) (int-as-set x2) x2)]
-;;           (cond (and (set? x1) (set? x2) 
-        
+(defn ordered-pair-destructure [x]
+  (let [s (if (number? x) (into #{} (range x)) x)]
+    (cond
+     (empty? s) [0 0]
+     (= (count s) 1) [(first s) (first s)]
+     :else (let [[v w] (take 2 s)]
+             [(first (gen-intersect-2 v w))
+              (first (exclusive-or-2 v w))]))))
+;;(defn 
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn vec-drop [k v]
+  (subvec v 0 (- (count v) k)))
+
+(defn left-pad [size stack]
+  (into [] (concat (repeat (- size (count stack)) 0) stack)))
+
+(defn stack-fn [f arity]
+  (fn [stack]
+    (let [stack0 (if (> arity (count stack)) (left-pad arity stack) stack)
+          idx (- (count stack0) arity)
+          args (subvec stack0 idx)
+          out (apply f args)]
+      (if (vector? out)
+        (into (subvec stack 0 idx) out)
+        (conj (subvec stack 0 idx) out)))))
+
+(defn rotate [stack direction]
+  (let [k (card (last stack))
+        stack0 (if (>= k (count stack))
+                 (left-pad k (butlast stack))
+                 (subvec stack 0 (dec (count stack))))
+        idx (- (count stack0) k)]
+    (cond
+     (= direction :right)
+       (into (conj (subvec stack0 0 idx) (last stack0))
+             (subvec stack0 idx (- (count stack0) 1)))
+     (= direction :left)
+       (into (subvec stack0 0 idx)
+             (conj (subvec stack0 (inc idx)) (nth stack0 idx))))))
+
+(defn read-1 [stack]
+  (let [c (.read *in*)]
+    (conj stack c)))
+
+(defn write-1 [ch]
+  (.write *out* (card ch))
+  [])
+
+(declare run-code)
+
+(defn conditionally-run-code [stack code]
+  (let [[then-code else-code] (clj-string/split code #",")
+        else-code (or else-code "")
+        bool (is-empty-set (last stack))]
+    (if bool
+      (run-code stack else-code)
+      (run-code stack then-code))))
+
+(defn loop-with-code [stack code]
+  (loop [st stack]
+    (if (is-empty-set (last st))
+      st
+      (recur (run-code st code)))))
+
+;; Do the union. This is the last part. 
+(defn set-comprehension [stack code]
+  (let [x       (last stack)
+        stack-1 (into [] (butlast stack))
+        s       (if (number? x) (into #{} (range x)) x)
+        f       (fn [x] (last (run-code (conj stack-1 x) code)))]
+    (conj stack-1 (shrink-if-possible (gen-union (map f x))))))
+
+(defn run-token [stack token]
+  (if (string? token)
+    (cond
+      (= (.charAt token 0) \()
+        (conditionally-run-code stack (.substring token 1 (dec (count token))))
+      (= (.charAt token 0) \[)
+        (loop-with-code stack (.substring token 1 (dec (count token))))
+      (= (.charAt token 0) \{)
+        (set-comprehension stack (.substring token 1 (dec (count token))))) 
+    (case token
+      \0 (conj stack 0)
+      \\ ((stack-fn choice 1) stack)
+      \/ ((stack-fn insert 2) stack)
+      \+ ((stack-fn pair 2) stack)
+      \_ ((stack-fn (fn [_] []) 1) stack)
+      \~ ((stack-fn (fn [x] [x x]) 1) stack)
+      \' ((stack-fn (fn [x] (insert x x)) 1) stack)
+      \# ((stack-fn card 1) stack)
+      \^ ((stack-fn power-set 1) stack)
+      \= ((stack-fn (fn [x y] (if (equality-check x y) 1 0)) 2) stack)
+      \? ((stack-fn (fn [x e] (if (member-check x e) 1 0)) 2) stack)
+      \> (rotate stack :right)
+      \< (rotate stack :left)
+      \$ (conj stack omega)
+      \` ((stack-fn choose-many 2) stack)
+      \& ((stack-fn (fn [x y] (gen-intersect-2 x y)) 2) stack)
+      \| ((stack-fn (fn [x y] (gen-union-2 x y)) 2) stack)
+      \- ((stack-fn (fn [x y] (difference-2 x y)) 2) stack)
+      \. ((stack-fn (fn [x y] (exclusive-or-2 x y)) 2) stack)
+      \" ((stack-fn (fn [x] (normalize #{x})) 1) stack)
+      \; ((stack-fn (fn [x y] x) 1) stack)
+      \% ((stack-fn ordered-pair 2) stack)
+      \* ((stack-fn ordered-pair-destructure 1) stack)
+      \! ((stack-fn write-1 1) stack)
+      \@ (read-1 stack))))
+
+(def single-char-token (set "~`!@#$%^&*0-_+=|\\:;\"'<>,.?/"))
+
+(defn parse-chunk [code-string idx]
+  (let [end (count code-string)
+        [open close] (case (.charAt code-string idx)
+                       \[ [\[ \]]
+                       \{ [\{ \}]
+                       \( [\( \)])]
+    (loop [cnt 1 i (inc idx)]
+      (cond (= cnt 0)  [(.substring code-string idx i) i]
+            (>= i end) (throw (Exception. "parsing error"))
+            :else (condp = (.charAt code-string i)
+                    open  (recur (inc cnt) (inc i))
+                    close (recur (dec cnt) (inc i))
+                    (recur cnt (inc i)))))))
+
+(defn parse-token [code-string & [idx]]
+  (let [idx (or idx 0)]
+    (if (>= idx (count code-string))
+      nil
+      (let [c (.charAt code-string idx)]
+        (cond (single-char-token c) [c (+ idx 1)]
+              (= c \[) (parse-chunk code-string idx)
+              (= c \() (parse-chunk code-string idx)
+              (= c \{) (parse-chunk code-string idx)
+              :else (throw (Exception. "parsing error")))))))
+
+(defn run-code [stack code]
+  (loop [stack-state stack idx 0]
+    (if-let [[tok idx-1] (parse-token code idx)]
+      (recur (run-token stack-state tok) idx-1)
+      stack-state)))
+
+(defn set-to-string [x numeric?]
+  (if numeric?
+    (if (do (number? x))
+      x
+      (format "{%s}" (clj-string/join ", " (map #(set-to-string % true) x))))
+    (let [s (if (number? x) (into #{} (range x)) x)]
+      (apply str (concat '("{") (map #(set-to-string % false) s) '("}"))))))
+
+(defn generate-test-case [n & [temp]]
+  (let [temp (or temp (inc n))]
+    (if (< (rand) (/ 1.0 temp))
+      n
+      (let [k (rand-int n)]
+        (hash-set (generate-test-case k (rand-int k))
+                  (generate-test-case (- n k) (rand-int (- n k))))))))
+
